905d0b33c64176eff26eb4895a62c1049964df2a
Mads Illemann <mads849h@edu.eal.dk>
2019-12-28 12:43:08 +0100

    Opdated some packages.

--- DrivingNote/ClientApp/src/app/home/home.component.ts
+++ DrivingNote/ClientApp/src/app/home/home.component.ts
@@ -1,17 +1,22 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { ModalComponent } from '../CustomComponents/modal/modal.component';
+import * as jsPDF from 'jspdf'
 
 @Component({
-    selector: 'app-home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.scss']
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
 })
 /** Home component*/
-export class HomeComponent {
-    /** Home ctor */
-    constructor() {
+export class HomeComponent implements OnInit {
+  /** Home ctor */
+  constructor() {
 
+  }
+
+  ngOnInit(): void {
+    this.CreatejsPdf();
   }
 
   //To get the methods from a child component.
@@ -25,6 +30,7 @@ export class HomeComponent {
     sport: new FormControl('', Validators.required),
   });
 
+
   SendNote() {
 
     if (this.infoForm.invalid)
@@ -34,6 +40,14 @@ export class HomeComponent {
     this.modal.CloseModal();
   }
 
+  CreateThePDF() {
+    var doc = new jsPDF();
+    doc.text('Hello world!', 10, 10)
+    doc.save('a4.pdf')
+  }
+
+
+  //Getters for error validations
   get getName() { return this.infoForm.get('name'); }
   get getLastname() { return this.infoForm.get('lastname'); }
   get getMail() { return this.infoForm.get('mail'); }
