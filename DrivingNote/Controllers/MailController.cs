43c9e1773df5ff71cf03e6c780c5b1a7653d0e43
Mads Illemann <mads849h@edu.eal.dk>
2019-12-29 20:03:26 +0100

    Mail controller added

---
+++ DrivingNote/Controllers/MailController.cs
@@ -0,0 +1,25 @@
+﻿namespace DrivingNote.Controllers
+{
+    using DrivingNote.Models;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MailController : ControllerBase
+    {
+        public MailController()
+        {
+
+        }
+
+
+
+        IActionResult SendMail(DrivingNote drivingNote)
+        {
+
+            return Ok();
+        }
+
+
+    }
+}
