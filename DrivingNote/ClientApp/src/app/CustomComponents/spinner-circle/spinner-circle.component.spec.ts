8c89891efe3d7abe6210ec2109d5d01567cb9077
Mads Illemann <mads849h@edu.eal.dk>
2020-02-14 15:14:45 +0100

    Spinner works like a charm

---
+++ DrivingNote/ClientApp/src/app/CustomComponents/spinner-circle/spinner-circle.component.spec.ts
@@ -0,0 +1,25 @@
+﻿/// <reference path="../../../../../node_modules/@types/jasmine/index.d.ts" />
+import { TestBed, async, ComponentFixture, ComponentFixtureAutoDetect } from '@angular/core/testing';
+import { BrowserModule, By } from "@angular/platform-browser";
+import { SpinnerCircleComponent } from './spinner-circle.component';
+
+let component: SpinnerCircleComponent;
+let fixture: ComponentFixture<SpinnerCircleComponent>;
+
+describe('spinnerCircle component', () => {
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ SpinnerCircleComponent ],
+            imports: [ BrowserModule ],
+            providers: [
+                { provide: ComponentFixtureAutoDetect, useValue: true }
+            ]
+        });
+        fixture = TestBed.createComponent(SpinnerCircleComponent);
+        component = fixture.componentInstance;
+    }));
+
+    it('should do something', async(() => {
+        expect(true).toEqual(true);
+    }));
+});
---
+++ DrivingNote/ClientApp/src/app/CustomComponents/spinner-circle/spinner-circle.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: 'app-spinner-circle',
+    templateUrl: './spinner-circle.component.html',
+    styleUrls: ['./spinner-circle.component.scss']
+})
+/** spinnerCircle component*/
+export class SpinnerCircleComponent {
+
+  @Input() text: string = "Loading";
+  @Input() show: boolean = true;
+  @Input() circleColor: string;
+    /** spinnerCircle ctor */
+    constructor() {
+
+    }
+}
