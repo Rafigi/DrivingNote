1a37dfacec407386bcad4778a27ba66a4fb841db
Mads Illemann <mads849h@edu.eal.dk>
2020-02-23 15:12:40 +0100

    model for json file.

---
+++ DrivingNote/Models/MailInformation.cs
@@ -0,0 +1,8 @@
+﻿namespace DrivingNote.Models
+{
+    public class MailInformation
+    {
+        public string ToMail { get; set; }
+        public string NameOfAttachedFile { get; set; }
+    }
+}
